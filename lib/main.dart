915c201488a788d77ca0458b852f6fbb4f5a0c00
CodeWithEyeasin <mreyeasinarafat@gmail.com>
2023-12-16 01:10:08 +0600

    text

--- lib/main.dart
+++ lib/main.dart
@@ -26,8 +26,28 @@ class HomeScreen extends StatelessWidget
     // TODO: implement build
     return Scaffold(
       backgroundColor: Color(0xFF10c419),
+      appBar: AppBar(
+        leading: Icon(Icons.home, color: Colors.black54, size: 30,),//home icon
+        title: Text('Home',
+        style: TextStyle(
+            color: Colors.black,
+        ),
+        ),
+        backgroundColor: Colors.amber,
+        elevation: 5,//shadow use
+
+      ),
       body: Center(
-        child: Text('Hello Flutter, My name is Eyeasin Arafat',
+        //child: Image.network('https://www.univariety.com/blog/wp-content/uploads/2014/08/motivational-goals.jpg'),)
+          child: Image.asset('Image/uncle Bob.jpg',
+            height: 300,
+            width: 300,
+            fit: BoxFit.cover,
+          //repeat: ImageRepeat.repeat,
+
+          ),
+      /* Center(
+       child: Text('Hello Flutter, My name is Eyeasin Arafat',
           textAlign: TextAlign.center,
           maxLines: 2,
           overflow: TextOverflow.ellipsis,
@@ -40,6 +60,8 @@ class HomeScreen extends StatelessWidget
           wordSpacing: 1,
           letterSpacing: 2,
         ),),
+
+      ),*/
       ),
     );
   }
