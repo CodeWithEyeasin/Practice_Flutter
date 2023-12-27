1529e295a09a5a05f16e6958a8ed76963a89cb17
CodeWithEyeasin <mreyeasinarafat@gmail.com>
2023-12-27 16:11:37 +0600

    new

--- lib/main.dart
+++ lib/main.dart
@@ -25,26 +25,82 @@ class HomeScreen extends StatelessWidget
   Widget build(BuildContext context) {
     // TODO: implement build
     return Scaffold(
-      backgroundColor: Color(0xFF10c419),
       appBar: AppBar(
-        leading: Icon(Icons.home, color: Colors.black54, size: 30,),//home icon
-        title: Text('Home',
-        style: TextStyle(
-            color: Colors.black,
-        ),
-        ),
-        backgroundColor: Colors.amber,
-        elevation: 5,//shadow use
+        title: Text('Home',),
       ),
-      body: Center(
-        //child: Image.network('https://www.univariety.com/blog/wp-content/uploads/2014/08/motivational-goals.jpg'),)
-          child: Image.asset('Image/uncle Bob.jpg',
-            height: 300,
-            width: 300,
-            fit: BoxFit.cover,
-          //repeat: ImageRepeat.repeat,
-          ),
-      ),
+    body:Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      //crossAxisAlignment: CrossAxisAlignment.end,
+      //mainAxisSize: MainAxisSize.min,
+      children: [
+        RichText(text: TextSpan(
+            text: ' Hello ostad',
+            style: TextStyle(
+              color: Colors.blue,
+              fontSize: 12,
+
+            ),
+          children: [
+            TextSpan(
+              text: ' i am',
+              style: TextStyle(
+                color: Colors.amber,
+                fontSize: 15,
+              )
+            ),
+            TextSpan(
+              text: ' eyeasin',
+              style: TextStyle(
+              color: Colors.blue,
+              fontSize: 19,
+                fontWeight: FontWeight.bold,
+                decoration: TextDecoration.underline
+            )
+            )
+          ]
+        )),
+        Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          //crossAxisAlignment: CrossAxisAlignment.end,
+          //mainAxisSize: MainAxisSize.min,
+          children: [
+            Text('Hello'),
+            Text("World"),
+
+            ElevatedButton(onPressed: () {
+              //showAboutDialog(context: context);
+              // showDialog(context: context, builder: (context)
+              // {
+              //   return //Text('Hello App');
+              //   AlertDialog(
+              //     title: Text('Message'),
+              //     content: Text('Hello welcome to app'),
+              //     actions: [
+              //       ElevatedButton(onPressed: () {}, child: Text('cancel')),
+              //     ],
+              //
+              //   );
+              // });
+              showModalBottomSheet(
+                  //isDismissible: false,
+                backgroundColor: Colors.blue,
+                  barrierColor: Colors.amber,
+                  context: context, builder: (context)
+              {
+                return //Text('Hello App');
+                  Column(
+                    children: [
+                      Text('hello'),
+                    ],
+                  );
+              });
+            }, child: Text('Tab Here')),
+          ],
+        ),
+      ],
+
+    ),
+
     );
   }
 
